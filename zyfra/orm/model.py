d59a873ae56ad09fbdd185344357304a56617c10
Nicolas DS <zyphos@zyphos.be>
2012-12-04 17:04:36 +0100

    [ADD] orm + singleton

---
+++ zyfra/orm/model.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Model(object):
+    def __init__(self, cr):
+        pass
---
+++ zyfra/orm/transaction.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Transaction(object):
+    __metaclass__ = Singleton
+    
+    pass
