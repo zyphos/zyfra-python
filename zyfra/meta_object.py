0520098bded8bd6b88bd05049a968655ab83f47a
Nicolas DS <zyphos@zyphos.be>
2013-03-04 17:06:26 +0100

    [IMP] Metaobject, support __method__

--- zyfra/meta_object.py
+++ zyfra/meta_object.py
@@ -3,5 +3,5 @@
 
 class MetaObject(object):
     def __new__(cls, value):
-        dct = dict((name, value) for name, value in cls.__dict__.items() if not name.startswith('__'))
+        dct = dict((name, value) for name, value in cls.__dict__.items() if name not in ['__new__']) #if not name.startswith('__')
         return type(cls.__name__ + '.' + value.__class__.__name__, (value.__class__, ), dct)(value)
