3d2d6d437c01123122ab96e72b711a9bfa4fa01e
Nicolas DS <zyphos@zyphos.be>
2013-03-04 17:06:57 +0100

    [IMP] Scraper, function

--- zyfra/scraper.py
+++ zyfra/scraper.py
@@ -11,11 +11,11 @@ from meta_object import MetaObject
 def toData(obj):
     if isinstance(obj, tuple):
         r = []
-        for i in range(len(obj)):
+        for i in xrange(len(obj)):
             r.append(toData(obj[i]))
         obj = tuple(r)
     elif isinstance(obj, list):
-        for i in range(len(obj)):
+        for i in xrange(len(obj)):
             obj[i] = toData(obj[i])
     elif isinstance(obj, dict):
         for i in obj.keys():
@@ -27,19 +27,27 @@ def toData(obj):
 class Data(MetaObject):
     def __new__(cls, value):
         obj = MetaObject.__new__(cls, value)
-        if (hasattr(obj, 'xpath')):
-            obj.__xpath = obj.xpath 
+        if (hasattr(value, 'xpath')):
+            obj.__xpath = value.xpath 
         return obj
+    
+    def __str__(self):
+        if(isinstance(self, lxml.etree._ElementTree) or isinstance(self, lxml.etree._Element)):
+            return lxml.etree.tostring(self)
+        return str(self)
         
     def re(self, regex):
         if(not isinstance(self, basestring)): 
+            if(isinstance(self, lxml.etree._ElementTree) or isinstance(self, lxml.etree._Element)):
+                res = re.findall(regex, str(self))
+                return toData(res)
             raise Exception('Can not do regex on non string object')
         res = re.findall(regex, self)
         return toData(res)
     
     def xpath(self, xpath):
-        if(isinstance(self, lxml.etree._ElementTree)):
-            return Data(self.xpath(xpath))
+        if(isinstance(self, lxml.etree._ElementTree) or isinstance(self, lxml.etree._Element)):
+            return toData(self.__xpath(xpath))
         if(not isinstance(self, basestring)): 
             raise Exception('Can not do xpath on this kind of object [' + str(self) + ']')
         tree = fromstring(self)
