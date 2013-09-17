4d5242c168326843da7980f9f4d1e1f4b308fe79
Nicolas DS <zyphos@zyphos.be>
2013-09-17 16:34:38 +0200

    [ADD] Singleton as decorator

--- zyfra/singleton.py
+++ zyfra/singleton.py
@@ -10,3 +10,13 @@ class Singleton(type):
         if cls.instance is None:
             cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
         return cls.instance
+
+
+# Source: https://wiki.python.org/moin/PythonDecoratorLibrary#The_Sublime_Singleton
+# @singleton
+# class Highlander:
+#     x = 100"""
+def singleton(cls):
+    instance = cls()
+    instance.__call__ = lambda: instance
+    return instance
