895d9b6bfea454eb5828b693962cc88586c3454f
Nicolas DS <zyphos@zyphos.be>
2014-02-11 14:38:52 +0100

    [IMP] Web_browser: support POST raw data, and added HEADER

--- zyfra/web_browser.py
+++ zyfra/web_browser.py
@@ -8,17 +8,19 @@ class WebBrowser(object):
     def __init__(self):
         self.cookie = None
     
-    def __call__(self, url, get_data=None, post_data=None):
+    def __call__(self, url, get_data=None, post_data=None, raw_data=None, header=None):
         # This method is session aware
         if get_data:
             url += '&' + urllib.urlencode(get_data)
         data_url = None
         if post_data:
             data_url = urllib.urlencode(post_data)
-        rq = urllib2.Request(url)
+        if raw_data:
+            data_url = raw_data
+        rq = urllib2.Request(url, data_url, header)
         if self.cookie:
             rq.add_header('cookie', self.cookie)
-        r = urllib2.urlopen(rq, data_url)
+        r = urllib2.urlopen(rq)
         cookie = r.headers.get('Set-Cookie')
         if cookie:
             self.cookie = cookie.split(';')[0]
