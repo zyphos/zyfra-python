30060b2a42b3fb222269073c8a62b7f602c6cc2a
Nicolas DS <zyphos@zyphos.be>
2014-11-24 14:44:10 +0100

    [IMP] Scrapper usage improvement

--- zyfra/__init__.py
+++ zyfra/__init__.py
@@ -1,16 +1,9 @@
 from zyfra_object import *
-import thread_rpc
 from web_browser import *
 from meta_object import *
-from scraper import *
-import config
-import tools
-import orm
 from wiki_parser import *
 from oo_json_rpc import OoJsonRPC
-import import_oo
 from pool import Pool
-import xml2dict
 from sms import Sms
 from zemail import Email
 # import monitor
--- zyfra/scraper.py
+++ zyfra/scraper.py
@@ -58,7 +58,10 @@ class Data(MetaObject):
         else:
             tree = fromstring(self)
         #self.__class__(
-        res = tree.xpath(xpath)
+        try:
+            res = tree.xpath(xpath)
+        except lxml.etree.XPathEvalError:
+            raise Exception('Invalid xpath expression: %s' % xpath)
         if isinstance(res, lxml.etree._ElementStringResult):
             res = str(res)
         return toData(res)
@@ -69,3 +72,88 @@ class Scraper(object):
  
     def get_url(self, *args, **kargs):
         return Data(self.web_browser(*args, **kargs))
+
+class Field(object):
+    xpath = None
+    
+    def __init__(self, xpath = None, full_xpath = None):
+        if full_xpath is not None:
+            self._xpath = xpath
+        elif xpath is not None:
+            self._auto_xpath(xpath)
+    
+    def _auto_xpath(self, xpath):
+            self._xpath = '//' + xpath
+
+    def __call__(self, data):
+        if self._xpath is not None:
+            return self.parse_value(data.xpath(self._xpath))
+
+    def parse_value(self, value):
+        return value
+
+class Text(Field):
+    def parse_value(self, value):
+        if isinstance(value, basestring):
+            return value
+        if isinstance(value, list) and len(value):
+            return self.parse_value(value[0])
+        if isinstance(value, lxml.etree.ElementBase):
+            return lxml.etree.tostring(value, pretty_print=True)
+        #return value.tostring()
+        #return dir(value)
+        return str(value)
+    pass
+
+class Int(Field):
+    def parse_value(self, value):
+        return int(value)
+
+class Float(Field):
+    def parse_value(self, value):
+        return float(value.replace(',', '.'))
+
+class Object(Field):
+    def __init__(self, xpath = None):
+        Field.__init__(self, xpath)
+        self._columns = {}
+        for col in dir(self):
+            attr = getattr(self, col)
+            if isinstance(attr, Field):
+                name = col.lower()
+                self._columns[name] = attr
+    
+    def parse_value(self, value):
+        res = {}
+        for field_name in self._columns:
+            res[field_name] = self._columns[field_name](value)
+        return res
+
+class Objects(Object):
+    def __call__(self, data):
+        res = []
+        if self._xpath is not None:
+            records = data.xpath(self._xpath)
+            if not isinstance(records, list):
+                raise Exception('Objects no list result')
+            for rec in data.xpath(self._xpath):
+                res.append(self.parse_value(rec))
+        return res
+
+class Page(Object):
+    url = None
+    
+    def __init__(self, web_browser = None):
+        if web_browser is None:
+            self._web_browser = WebBrowser()
+        else:
+            self._web_browser = browser
+        Object.__init__(self)
+    
+    def __call__(self, url=None):
+        if url is None:
+            url = self.url
+        if url is None:
+            raise Exception('No url defined !')
+        data = Data(self._web_browser(url))
+        return self.parse_value(data)
