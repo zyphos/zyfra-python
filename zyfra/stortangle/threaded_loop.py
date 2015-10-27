1f5450a2de7ad3a459ee48272f1fc2e96cfac435
Zyphos <zyphos@zyphos.be>
2015-10-27 23:24:34 +0100

    [ADD] Threaded loop

---
+++ zyfra/stortangle/threaded_loop.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Threaded loop that can be interrupted externaly
+"""
+import threading
+
+class ThreadedLoop(object):
+    
+    def __init__(self, loop_sleep_delay=0.1):
+        self.__running_event = threading.Event()
+        self.__starting_event = threading.Event()
+        self.__running_event.clear()
+        self.__thread = None
+        self.__loop_sleep_delay = loop_sleep_delay
+    
+    def _init(self): # Setup all data before looping
+        pass
+    
+    def _loop(self): # This is called at each loop iteration
+        pass
+    
+    def _clear(self): # Called when the loop ended
+        pass
+    
+    def start(self, wait_started=False):
+        if self.__running_event.is_set(): # Only run the thread once
+            return
+        self.__starting_event.clear()
+        self.__thread = threading.Thread(target=self.__the_loop)
+        self.__thread.start()
+        while wait_started and not self.__starting_event.is_set():
+            pass
+    
+    def stop(self):
+        self.__running_event.clear()
+    
+    def join(self):
+        self.stop()
+        self.__thread.join()
+    
+    def is_running(self):
+        return self.__running_event.is_set()
+    
+    def __the_loop(self):
+        self.__running_event.set()
+        self._init()
+        while self.__running_event.is_set():
+            self._loop()
+            time.sleep(self.__loop_sleep_delay)
+        self._clear()
+        self.__running_event.clear()
