4b0e2a1b0caaa1b6a46709a3f4f1e5ec47810a97
Zyphos <zyphos@zyphos.be>
2015-10-27 22:45:13 +0100

    [REF] MessageQueue

--- zyfra/message_queue.py
+++ zyfra/message_queue.py
@@ -17,6 +17,7 @@ class MyQueue(MessageQueue):
 
 import sqlite3
 import threading
+import os
 from datetime import datetime
 
 from tools import DictObject
@@ -67,35 +68,34 @@ class Cursor(object):
     #    self.__lock.release()
 
 class MessageQueue(object):
-    _columns = None
-    _db = None
+    _db_name = 'message_queue.db'
+    _table_name = 'queue'
     id = FieldInt(primary_key=True)
     date = FieldText()
     treated = FieldInt()
     
-    def __init__(self, name, db_name='message_queue.db'):
+    def __init__(self):
         self._lock = threading.Lock()
-        self.name = name
-        self._columns = {}
-        self._columns_order = []
+        self.__columns = {}
+        self.__columns_order = []
         for col in dir(self):
             attr = getattr(self, col)
             if isinstance(attr, Field):
                 col_name = col.lower()
-                self._columns[col_name] = attr
-                self._columns_order.append(col_name)
-        self._db = sqlite3.connect(db_name)
+                self.__columns[col_name] = attr
+                self.__columns_order.append(col_name)
+        self.__db = sqlite3.connect(self._db_name)
         self.create_table()
     
     def create_table(self):
         with self.get_cursor() as cr:
             columns = []
-            for column in self._columns_order:
-                columns.append('%s %s' % (column, self._columns[column].get_type()))
-            cr.execute('CREATE TABLE IF NOT EXISTS %s (%s)' % (self.name, ','.join(columns)))
+            for column in self.__columns_order:
+                columns.append('%s %s' % (column, self.__columns[column].get_type()))
+            cr.execute('CREATE TABLE IF NOT EXISTS %s (%s)' % (self._table_name, ','.join(columns)))
     
     def get_cursor(self):
-        return Cursor(self._db, self._lock)
+        return Cursor(self.__db, self._lock)
     
     def get_count(self):
         with self.get_cursor() as cr:
@@ -106,7 +106,7 @@ class MessageQueue(object):
     def add(self, **kargs):
         #Ignore unknown fields
         for key in kargs.keys():
-            if key not in self._columns or key in ['id']:
+            if key not in self.__columns or key in ['id']:
                 del kargs[key]
         if 'date' not in kargs:
             kargs['date'] = str(datetime.now())
@@ -115,43 +115,46 @@ class MessageQueue(object):
         values = []
         for column, value in kargs.iteritems():
             columns.append(column)
-            values.append(self._columns[column].sqlify(value))
+            values.append(self.__columns[column].sqlify(value))
         with self.get_cursor() as cr:
-            sql = "INSERT INTO %s (%s) VALUES (%s)" % (self.name, ','.join(columns), ','.join(values))
+            sql = "INSERT INTO %s (%s) VALUES (%s)" % (self._table_name, ','.join(columns), ','.join(values))
             #print sql
             cr.execute(sql)
     
     def _where2sql(self, where):
         wheresql = []
         for key, value in where.iteritems():
-            if key not in self._columns:
+            if key not in self.__columns:
                 continue
-            wheresql.append('%s=%s' % (key, self._columns[key].sqlify(value)))
+            wheresql.append('%s=%s' % (key, self.__columns[key].sqlify(value)))
         return ' AND '.join(wheresql)
     
     def delete(self, **where):
         wheresql = self._where2sql(where)
         with self.get_cursor() as cr:
-            cr.execute("DELETE FROM %s WHERE %s" % (self.name, wheresql))
+            cr.execute("DELETE FROM %s WHERE %s" % (self._table_name, wheresql))
     
     def mark_as_treated(self, **where):
         wheresql = self._where2sql(where)
         if wheresql == '':
             return
         with self.get_cursor() as cr:
-            cr.execute("UPDATE %s SET treated=1 WHERE %s" % (self.name, wheresql))
+            cr.execute("UPDATE %s SET treated=1 WHERE %s" % (self._table_name, wheresql))
     
     def get_next(self, **where):
         wheresql = self._where2sql(where)        
         if wheresql:
             wheresql = 'AND %s' % wheresql
         with self.get_cursor() as cr:
-            cr.execute("SELECT %s FROM %s WHERE treated=0 %s ORDER BY date,id LIMIT 1" % (','.join(self._columns_order), self.name, wheresql))
+            cr.execute("SELECT %s FROM %s WHERE treated=0 %s ORDER BY date,id LIMIT 1" % (','.join(self.__columns_order), self._table_name, wheresql))
             res = cr.fetchone()
         if res is None:
             return None
-        return DictObject(dict(zip(self._columns_order, res)))
+        return DictObject(dict(zip(self.__columns_order, res)))
     
     def prune_treated(self):
         with self.get_cursor() as cr:
-            cr.execute("DELETE FROM %s WHERE treated=1" % (self.name, ))
+            cr.execute("DELETE FROM %s WHERE treated=1" % (self._table_name, ))
+    
+    def delete_db(self):
+        os.unlink(self._db_name)
