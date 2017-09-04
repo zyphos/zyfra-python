4c88178a2150609bc999b1e59939b24e64267ff4
Nicolas DS <zyphos@zyphos.be>
2017-09-04 16:57:19 +0200

    [IMP] tools: add rate_limited decorator

--- zyfra/tools.py
+++ zyfra/tools.py
@@ -443,6 +443,36 @@ def mail_on_exception(subject, from_email=None, to_email=None, catch=False, show
         return _fn
     return decorator
 
+
+# Source: https://gist.github.com/gregburek/1441055
+def rate_limited(max_per_second):
+    """
+    Decorator that make functions not be called faster than
+    """
+    lock = threading.Lock()
+    min_interval = 1.0 / float(max_per_second)
+
+    def decorate(func):
+        last_time_called = [0.0]
+
+        @wraps(func)
+        def rate_limited_function(*args, **kwargs):
+            lock.acquire()
+            elapsed = time.clock() - last_time_called[0]
+            left_to_wait = min_interval - elapsed
+
+            if left_to_wait > 0:
+                time.sleep(left_to_wait)
+
+            lock.release()
+            ret = func(*args, **kwargs)
+            last_time_called[0] = time.clock()
+            return ret
+
+        return rate_limited_function
+
+    return decorate
+
 class ShowProgress(object):
     """ This class show progress of a process, and estimated time of arrival
         Usage:
