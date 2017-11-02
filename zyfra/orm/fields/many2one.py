c6376c5203135ed8f803806b4009ff09d1756565
Nicolas DS <zyphos@zyphos.be>
2017-11-02 10:48:46 +0100

    [REF] ORM: many2one

--- zyfra/orm/fields/many2one.py
+++ zyfra/orm/fields/many2one.py
@@ -72,9 +72,15 @@ class Many2One(Relational):
                 op_data = context['op_data']
                 ta = sql_query.get_new_table_alias()
                 if operator == 'parent_of':
-                    sql = 'EXISTS(SELECT ' + obj._key_sql_name + ' FROM ' + obj._table + ' AS ' + ta + ' WHERE ' + ta + '.' + obj._key_sql_name + '=' + op_data + ' AND ' + pa + '.' + self.pleft + '<' + ta + '.' + self.pleft + ' AND ' + pa + '.' + self.pright + '>' + ta + '.' + self.pright + ')'
+                    sql = 'EXISTS(SELECT %s FROM %s AS %s WHERE %s.%s=%s AND %s.%s<%s.%s AND %s.%s>%s.%s' % (obj._key_sql_name, obj._table, ta,
+                                                                                                             ta, obj._key_sql_name, op_data,
+                                                                                                             pa, self.pleft, ta, self.pleft,
+                                                                                                             pa, self.pright, ta, self.pright)
                 elif operator == 'child_of':
-                    sql = 'EXISTS(SELECT ' + obj._key_sql_name + ' FROM ' + obj._table + ' AS ' + ta + ' WHERE ' + ta + '.' + obj._key_sql_name + '=' + op_data + ' AND ' + pa + '.' + self.pleft + '>' + ta + '.' + self.pleft + ' AND ' + pa + '.' + self.pright + '<' + ta + '.' + self.pright + ')'
+                    sql = 'EXISTS(SELECT %s FROM %s AS %s WHERE %s.%s=%s AND %s.%s>%s.%s AND %s.%s<%s.%s' % (obj._key_sql_name, obj._table, ta,
+                                                                                                             ta, obj._key_sql_name, op_data,
+                                                                                                             pa, self.pleft, ta, self.pleft,
+                                                                                                             pa, self.pright, ta, self.pright)
                 sql_query.order_by.append(pa + '.' + self.pleft)
                 return sql
             field_link = parent_alias.alias + '.' + self.sql_name
@@ -91,17 +97,17 @@ class Many2One(Relational):
                 if foreign_key in self.relation_object._columns:
                     foreign_key = '%ta%.' + foreign_key
                 if local_key in self.object._columns:
-                    local_key = palias + '.' + local_key
-                relations.append(foreign_key + '=' + local_key)
+                    local_key = '%s.%s' % (palias, local_key)
+                relations.append('%s=%s' % (foreign_key, local_key))
             sql_on = implode(' and ', relations)
         else:
-            sql_on = '%ta%.' + self.relation_object_sql_key + '=' + parent_alias.alias + '.' + self.sql_name
+            sql_on = '%%ta%%.%s=%s.%s' % (self.relation_object_sql_key, parent_alias.alias, self.sql_name)
         
-        sql = (not self.required and '' or 'LEFT ') + 'JOIN ' + robj._table + ' AS %ta% ON ' + sql_on
+        sql = '%sJOIN %s AS %%ta%% ON %s' % ((not self.required and '' or 'LEFT '), robj._table, sql_on)
         if sql_query.context.get('visible', True) and robj._visible_condition != '':
             sql_txt, on_condition = sql.split(' ON ')
             visible_sql_q = SQLQuery(robj, '%ta%')
-            sql = sql_txt + ' ON (' + on_condition + ')AND(' + visible_sql_q.where2sql('') + ')'
+            sql = '%s ON (%s)AND(%s)' % (sql_txt, on_condition, visible_sql_q.where2sql(''))
         ta = sql_query.get_table_alias(field_link, sql, parent_alias)
         field_name = fields.pop(0)
         field_name, field_param = sql_query.split_field_param(field_name)
@@ -141,21 +147,21 @@ class Many2One(Relational):
         for id, old_value in old_values.iteritems():
             if old_value == new_value:
                 continue
-            obj = db.get_object('SELECT ' + left_col + ' AS lc, ' + right_col + ' AS rc FROM ' + table + ' WHERE ' + self.object._key + '=' + id)
+            obj = db.get_object('SELECT %s AS lc, %s AS rc FROM %s WHERE %s=%s' % (left_col, right_col, table, self.object._key, id))
             l0 = obj.lc
             r0 = obj.rc
             d = r0 - l0
-            children_ids = db.get_array('SELECT ' + key + ' FROM ' + table + ' WHERE ' + left_col + '>=' + l0 + ' AND ' + right_col + '<=' + r0, key)
+            children_ids = db.get_array('SELECT %s FROM %s WHERE %s>=%s AND %s<=%s' % (key, table, left_col, l0, right_col, r0), key)
             l1 = self._tree_get_new_left(id, new_value)
             if (l1 > l0):
-                db.safe_query('UPDATE ' + table + ' SET ' + left_col + '=' + left_col + '-' + (d+1) + ' WHERE ' + left_col + '>' + r0 + ' AND ' + left_col + '<' + l1)
-                db.safe_query('UPDATE ' + table + ' SET ' + right_col + '=' + right_col + '-' + (d+1) + ' WHERE ' + right_col + '>' + r0 + ' AND ' + right_col + '<' + l1)
+                db.safe_query('UPDATE %s SET %s=%s-%s WHERE %s>%s AND %s<%s' % (table, left_col, left_col, (d+1), left_col, r0, left_col, l1))
+                db.safe_query('UPDATE %s SET %s=%s-%s WHERE %s>%s AND %s<%s' % (table, right_col, right_col, (d+1), right_col, r0, right_col, l1))
                 delta = l1 - l0 - d - 1
             else:
-                db.safe_query('UPDATE ' + table + ' SET ' + left_col + '=' + left_col + '+' + (d+1) + ' WHERE ' + left_col + '>=' + l1 + ' AND ' + left_col + '<' + l0)
-                db.safe_query('UPDATE ' + table + ' SET ' + right_col + '=' + right_col + '+' + (d+1) + ' WHERE ' + right_col + '>=' + l1 + ' AND ' + right_col + '<' + l0)
+                db.safe_query('UPDATE %s SET %s=%s+%s WHERE %s>=%s AND %s<%s' % (table, left_col, left_col, (d+1), left_col, l1, left_col, l0))
+                db.safe_query('UPDATE %s SET %s=%s+%s WHERE %s>=%s AND %s<%s' % (table, right_col, right_col, (d+1), right_col, l1, right_col, l0))
                 delta = l1 - l0
-            db.safe_query('UPDATE ' + table + ' SET ' + left_col + '=' + left_col + '+' + delta + ',' + right_col + '=' + right_col + '+' + delta + ' WHERE ' + key + ' in %s', array(children_ids))
+            db.safe_query('UPDATE %s SET %s=%s+%s,%s=%s+%s WHERE %s IN %%s' % (table, left_col, left_col, delta, right_col, right_col, delta, key), [children_ids])
 
     def _tree_get_new_left(self, id, value):
         db = self.object._pool.db
@@ -165,15 +171,15 @@ class Many2One(Relational):
         table = self.object._table
         if value == null or value == 0:
             l1 = 1
-            brothers = self.object.select(key + ' AS id,' + right_col + ' AS rc WHERE ' + self.sql_name + ' IS NULL OR ' + self.sql_name + '=0')
+            brothers = self.object.select('%s AS id, %s AS rc WHERE %s IS NULL OR %s=0' % (key, right_col, self.sql_name, self.sql_name))
             for brother in brothers:
                 if brother.id == id:
                     break
                 l1 = brother.rc + 1
         else:
-            parent_obj = self.object._pool.db.get_object('SELECT ' + left_col + ' AS lc FROM ' + table + ' WHERE ' + key + '=%s', array(value))
+            parent_obj = self.object._pool.db.get_object('SELECT %s AS lc FROM %s WHERE %s=%%s' % (left_col, table, key), [value])
             l1 = parent_obj.lc + 1
-            brothers = self.object.select(key + ' AS id,' + right_col + ' AS rc WHERE ' + self.sql_name + '=%s', array(), array(value))
+            brothers = self.object.select('%s AS id, %s AS rc WHERE %s=%%s' % (key, right_col, self.sql_name), [], [value])
             for brother in brothers:
                 if brother.id == id:
                     break
@@ -187,14 +193,14 @@ class Many2One(Relational):
         right = left+1
 
         if id is None or id == 0:
-            rows = self.object.select(key + ' AS id WHERE ' + self.sql_name + ' IS NULL OR ' + self.sql_name + '=0')
+            rows = self.object.select('%s AS id WHERE %s IS NULL OR %s=0' % (key, self.sql_name, self.sql_name))
         else:
-            rows = self.object.select(key + ' AS id WHERE ' + self.sql_name + '=%s', array(), array(id))
+            rows = self.object.select('%s AS id WHERE %s=%%s' % (key, self.sql_name), [], [id])
         for row in rows:
             right = self.rebuild_tree(row.id, right, key, table)
         if id != 0 and id is not None:
             db = self.object._pool.db
-            db.safe_query('UPDATE ' + table + ' SET ' + self.pleft + '=' + left + ', ' + self.pright + '=' + right + ' WHERE ' + key + '=%s', array(id))
+            db.safe_query('UPDATE %s SET %s=%s, %s=%s WHERE %s=%%s' % (table, self.pleft, left, self.pright, right, key), [id])
         return right+1
 
     def before_unlink_trigger(self, old_values):
@@ -206,17 +212,17 @@ class Many2One(Relational):
         table = self.object._table
         left_col = self.pleft
         right_col = self.pright
-        sql = 'SELECT ' + self.pleft + ' AS pleft FROM ' + self.object._table + ' WHERE ' + self.object._key + ' IN %s ORDER BY pleft'
-        plefts = db.get_array(sql, 'pleft', '', array(array_keys(old_values)))
+        sql = 'SELECT %s AS pleft FROM %s WHERE %s IN %%s ORDER BY pleft' % (self.pleft, self.object._table, self.object._key)
+        plefts = db.get_array(sql, 'pleft', '', [old_values.keys()])
         for i in xrange(len(plefts)):
             nbi = (i + 1) * 2
             if i+1 < nb:
                 if plefts[i+1] - plefts[i] >= 2:
-                    db.safe_query('UPDATE ' + table + ' SET ' + left_col + '=' + left_col + '-' + nbi + ' WHERE ' + left_col + '>' + plefts[i] + ' AND ' + left_col + '<' + plefts[i+1])
-                    db.safe_query('UPDATE ' + table + ' SET ' + right_col + '=' + right_col + '-' + nbi + '  WHERE ' + right_col + '>' + plefts[i] + ' AND ' + right_col + '<' + plefts[i+1])
+                    db.safe_query('UPDATE %s SET %s=%s-%s WHERE %s>%s AND %s<%s' % (table, left_col, left_col, nbi, left_col, plefts[i], left_col, plefts[i+1]))
+                    db.safe_query('UPDATE %s SET %s=%s-%s WHERE %s>%s AND %s<%s' % (table, right_col, right_col, nbi, right_col, plefts[i], right_col, plefts[i+1]))
             else:
-                db.safe_query('UPDATE ' + table + ' SET ' + left_col + '=' + left_col + '-' + nbi + ' WHERE ' + left_col + '>' + plefts[i])
-                db.safe_query('UPDATE ' + table + ' SET ' + right_col + '=' + right_col + '-' + nbi + '  WHERE ' + right_col + '>' + plefts[i])
+                db.safe_query('UPDATE %s SET %s=%s-%s WHERE %s>%s' % (table, left_col, left_col, nbi, left_col, plefts[i]))
+                db.safe_query('UPDATE %s SET %s=%s-%s WHERE %s>%s' % (table, right_col, right_col, nbi, right_col, plefts[i]))
 
     def after_create_trigger(self, id, value, context):
         if not self.left_right:
@@ -226,6 +232,6 @@ class Many2One(Relational):
         left_col = self.pleft
         right_col = self.pright
         table = self.object._table
-        db.safe_query('UPDATE ' + table + ' SET ' + left_col + '=' + left_col + '+2  WHERE ' + left_col + '>=' + l1)
-        db.safe_query('UPDATE ' + table + ' SET ' + right_col + '=' + right_col + '+2  WHERE ' + right_col + '>=' + l1)
-        db.safe_query('UPDATE ' + table + ' SET ' + left_col + '=' + l1 + ', ' + right_col + '=' + (l1+1) + ' WHERE ' + self.object._key + '=%s', array(id))
+        db.safe_query('UPDATE %s SET %s=%s+2 WHERE %s >= %s' % (table, left_col, left_col, left_col, l1))
+        db.safe_query('UPDATE %s SET %s=%s+2 WHERE %s >= %s' % (table, right_col, left_col, right_col, l1))
+        db.safe_query('UPDATE %s SET %s=%s, %s=%s WHRE %s=%%s' % (table, left_col, l1, right_col, (l1+1), self.object._key), [id])
