a01b11e264769f84fa971131a5a7c0178dc34929
Nicolas DS <zyphos@zyphos.be>
2018-08-22 16:15:23 +0200

    [FIX] monitor: RAID status msg

--- zyfra/monitor/host_service.py
+++ zyfra/monitor/host_service.py
@@ -161,8 +161,9 @@ class raid(HostService):
             disk_ratio = rsplit[-2]
             status = rsplit[-1]
             i += 1
-            if rows[i].strip() != '':
+            if i < len(rows) and rows[i].strip() != '':
                 msg = rows[i].strip()
+                i += 1
             else:
                 msg = None
             raids[name] = {'composition': composition,
