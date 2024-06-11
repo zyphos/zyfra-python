ad3bea34b50bf14505c829a04ad1aace6c114d6f
Nicolas De Smet <ndesmet@matedex.be>
2024-06-11 16:50:04 +0200

    [IMP] Tools: better print table render

--- zyfra/tools.py
+++ zyfra/tools.py
@@ -342,24 +342,39 @@ def print_table(table, columns=None):
         print_table_list_dict(table, columns)
         return
 
+    # zip row and column if necessary
+    first_line_is_col = False
+    if len(columns) > len(table):
+        #print 'More columns[%s] than rows[%s] zip the table' % (len(columns), len(table))
+        lines = []
+        columns = list(columns)
+        for i in range(len(columns)):
+            lines.append([str(columns[i]),] + [str(r[i]) for r in table])
+    else:
+        first_line_is_col = True
+        lines = [columns[:]] # copy
+        for r in table:
+            lines.append(list(str(r[i]) for i in range(len(columns))))
+
     # compute column size
-    col_sizes = [len(c) for c in columns]
-    for row in table:
-        for i, value in enumerate(row):
+    col_sizes = [len(c) for c in lines[0]]
+    for line in lines:
+        for i, value in enumerate(line):
             length = len(repr(value))
             if length > col_sizes[i]:
                 col_sizes[i] = length
 
-    columns = [c.ljust(col_sizes[i]) for i, c in enumerate(columns)]
-    lines = ['-' * s for s in col_sizes]
-    for row in table:
-        for i, value in enumerate(row):
-            row[i] = repr(row[i]).ljust(col_sizes[i])
+    #columns = [c.ljust(col_sizes[i]) for i, c in enumerate(columns)]
+    #lines = ['-' * s for s in col_sizes]
+    for line in lines:
+        for i, value in enumerate(line):
+            #line[i] = repr(row[i]).ljust(col_sizes[i])
+            line[i] = value.ljust(col_sizes[i])
 
-    print('|'.join(columns))
-    print('|'.join(lines))
-    for row in table:
-        print('|'.join(row))
+    if first_line_is_col:
+        lines = [lines[0]]+[['-' * s for s in col_sizes]]+lines[1:]
+    for line in lines:
+        print('|'.join(line))
 
 def print_table_list_dict(data, columns=None):
     "Pretty print a list of dict"
@@ -377,8 +392,12 @@ def print_table_list_dict(data, columns=None):
         print('Sub data is empty')
         return
     if columns is None:
-        columns = data[0].keys()
-    rows = [[row[c] for c in columns] for row in data]
+        columns = set()
+        for row in data:
+            columns |= set(row.keys())
+    columns = list(columns)
+    columns.sort()
+    rows = [[row.get(c,'') for c in columns] for row in data]
     print_table(rows, columns)
 
 # source : https://wiki.python.org/moin/PythonDecoratorLibrary#Line_Tracing_Individual_Functions
